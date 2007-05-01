3ca747d694281f945bb469d15bc5697091fee042
olivier <>
2007-05-01 15:58:45 +0000

    added license headers + cleanup

--- telemeta/models.py
+++ telemeta/models.py
@@ -1,3 +1,12 @@
+# Copyright (C) 2007 Samalyse SARL
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution. The terms
+# are also available at http://svn.parisson.org/telemeta/TelemetaLicense.
+#
+# Author: Olivier Guilyardi <olivier@samalyse.com>
+
 import telemeta
 from django.db import models
 from django.db.models import Q
--- telemeta/urls.py
+++ telemeta/urls.py
@@ -1,36 +1,49 @@
+# Copyright (C) 2007 Samalyse SARL
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution. The terms
+# are also available at http://svn.parisson.org/telemeta/TelemetaLicense.
+#
+# Author: Olivier Guilyardi <olivier@samalyse.com>
+
 from django.conf.urls.defaults import *
 from telemeta.models import MediaItem, MediaCollection
 from telemeta.core import ComponentManager
 from telemeta.views.web import WebView
 
+# initialization
 comp_mgr = ComponentManager()
 web_view = WebView(comp_mgr)
 
-all_items = {
-    'queryset': MediaItem.objects.all(),
-}
-
-all_collections = {
-    'queryset': MediaCollection.objects.all(),
-}
+# query sets for Django generic views
+all_items = { 'queryset': MediaItem.objects.all(), }
+all_collections = { 'queryset': MediaCollection.objects.all(), }
 
 urlpatterns = patterns('',
     (r'^$', web_view.index),
+
+    # items
     (r'^items/$', 'django.views.generic.list_detail.object_list', 
         dict(all_items, paginate_by=10, template_name="mediaitem_list.html")),
     (r'^items/(?P<item_id>[0-9]+)/$', web_view.item_detail),
     (r'^items/(?P<item_id>[0-9]+)/download/(?P<format>[0-9A-Z]+)/$', 
         web_view.item_export),
-    #(r'^media_item/(?P<media_item_id>[0-9]+)/$', 'telemeta.views.web.media_item_edit'),
-    (r'^media_item/(?P<media_item_id>[0-9]+)/update/$', 'telemeta.views.web.media_item_update'),
 
+    # collections
     (r'^collections/$', 'django.views.generic.list_detail.object_list',
         dict(all_collections, paginate_by=10, template_name="collection_list.html")),
     (r'^collections/(?P<object_id>[0-9]+)/$', 
         'django.views.generic.list_detail.object_detail',
         dict(all_collections, template_name="collection_detail.html")),
-    #(r'^dictionaries/$', web_view.dictionary_list),        
+
+    # search
+    (r'^search/$', web_view.quick_search),
+
+    # administration        
     (r'^admin/$', web_view.admin_index),        
+
+    # dictionaries administrations
     (r'^admin/dictionaries/(?P<dictionary_id>[0-9a-z]+)/$', web_view.edit_dictionary),        
     (r'^admin/dictionaries/(?P<dictionary_id>[0-9a-z]+)/add/$', web_view.add_to_dictionary),        
     (r'^admin/dictionaries/(?P<dictionary_id>[0-9a-z]+)/update/$', web_view.update_dictionary),        
@@ -40,12 +53,12 @@ urlpatterns = patterns('',
     (r'^admin/dictionaries/(?P<dictionary_id>[0-9a-z]+)/(?P<value_id>[0-9]+)/update/$',
         web_view.update_dictionary_value),   
 
-    (r'^search/$', web_view.quick_search),
-
 
     # CSS+Images (FIXME: for developement only)
-    (r'^css/(?P<path>.*)$', 'django.views.static.serve', {'document_root': './telemeta/htdocs/css'}),
-    (r'^images/(?P<path>.*)$', 'django.views.static.serve', {'document_root': './telemeta/htdocs/images'}),
+    (r'^css/(?P<path>.*)$', 'django.views.static.serve', 
+        {'document_root': './telemeta/htdocs/css'}),
+    (r'^images/(?P<path>.*)$', 'django.views.static.serve', 
+        {'document_root': './telemeta/htdocs/images'}),
 )
 
 
