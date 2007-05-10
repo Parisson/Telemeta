2b3e09cf2b52cef610324334b938240197e9b8ce
olivier <>
2007-05-10 16:40:48 +0000

    add LEM collection fields

--- telemeta/models.py
+++ telemeta/models.py
@@ -40,27 +40,73 @@ class MediaCollectionManager(models.Manager):
     def quick_search(self, pattern):
         return super(MediaCollectionManager, self).get_query_set().filter(
             Q(title__icontains=pattern) |
-            Q(description__icontains=pattern)
+            Q(creator__icontains=pattern)
         )
 
 
 class MediaCollection(models.Model, MediaCore):
     "Group related media items"
 
+    publisher_reference = models.CharField(maxlength=250, blank=True)
+    physical_format = models.CharField(maxlength=250, blank=True)
+    id = models.CharField(maxlength=250, primary_key=True)
+    id.dc_element = 'identifier'
     title = models.CharField(maxlength=250)
-    date = models.DateField()
-    contributor = models.CharField(maxlength=250, blank=True)
-    coverage = models.CharField(maxlength=250, blank=True)
-    creator = models.CharField(maxlength=250, blank=True)
-    description = models.CharField(maxlength=250, blank=True)
-    format = models.CharField(maxlength=250, blank=True)
-    identifier = models.CharField(maxlength=250, blank=True)
-    language = models.CharField(maxlength=250, blank=True)
+    title.dc_element = 'title'
+    native_title = models.CharField(maxlength=250, blank=True)
+    native_title.dc_element = 'title'
+    physical_items_num = models.IntegerField(blank=True) 
+    publishing_status = models.CharField(maxlength=250, blank=True)
+    is_original = models.CharField(maxlength=250)
+    is_full_copy = models.CharField(maxlength=250)
+    copied_from = models.ForeignKey('self', null=True),
+    copied_from[0].dc_element = 'relation'
+    creator = models.CharField(maxlength=250)
+    creator.dc_element = 'creator'
+    booklet_writer = models.CharField(maxlength=250, blank=True)
+    booklet_writer.dc_element = 'contributor'
+    booklet_description = models.TextField(blank=True)
+    collector = models.CharField(maxlength=250, blank=True)
+    collector.dc_element = 'contributor'
     publisher = models.CharField(maxlength=250, blank=True)
+    publisher.dc_element = 'publisher'
+    date_published = models.IntegerField(blank=True)
+    date_published.dc_element = 'date'
+    publisher_collection = models.CharField(maxlength=250, blank=True)
+    publisher_serial_id = models.IntegerField(blank=True)
+    ref_biblio = models.CharField(maxlength=250, blank=True)
+    acquisition_mode = models.CharField(maxlength=250, blank=True)
+    comment = models.TextField(blank=True)
+    record_author = models.CharField(maxlength=250, blank=True)
+    record_writer = models.CharField(maxlength=250, blank=True)
     rights = models.CharField(maxlength=250, blank=True)
-    source = models.CharField(maxlength=250, blank=True)
-    subject = models.CharField(maxlength=250, blank=True)
-    physical_format = models.ForeignKey(PhysicalFormat, null=True, blank=True)
+    rights.dc_element = 'rights'
+    annee_enr = models.IntegerField(blank=True)
+    annee_enr.dc_element = 'date'
+    terrain_ou_autre = models.CharField(maxlength=250, blank=True)
+    duree_approx = models.CharField(maxlength=250, blank=True)
+    tri_dibm = models.IntegerField(blank=True)
+    travail = models.CharField(maxlength=250, blank=True)
+    compil_face_plage = models.CharField(maxlength=250, blank=True)
+    deposant_cnrs = models.CharField(maxlength=250, blank=True)
+    fiches = models.CharField(maxlength=250, blank=True)
+    a_informer = models.IntegerField(blank=True)
+    numerisation = models.CharField(maxlength=250, blank=True)
+    champ36 = models.CharField(maxlength=250, blank=True)
+     
+#    date = models.DateField()
+#    contributor = models.CharField(maxlength=250, blank=True)
+#    coverage = models.CharField(maxlength=250, blank=True)
+#    creator = models.CharField(maxlength=250, blank=True)
+#    description = models.CharField(maxlength=250, blank=True)
+#    format = models.CharField(maxlength=250, blank=True)
+#    identifier = models.CharField(maxlength=250, blank=True)
+#    language = models.CharField(maxlength=250, blank=True)
+#    publisher = models.CharField(maxlength=250, blank=True)
+#    rights = models.CharField(maxlength=250, blank=True)
+#    source = models.CharField(maxlength=250, blank=True)
+#    subject = models.CharField(maxlength=250, blank=True)
+#    physical_format = models.ForeignKey(PhysicalFormat, null=True, blank=True)
 
     objects = MediaCollectionManager()
 
