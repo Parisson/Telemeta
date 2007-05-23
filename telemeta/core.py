0790d98117ba2805d7e6c8f4361c436a7ef4ab5b
olivier <>
2007-05-23 18:53:35 +0000

    simplify !TelemetaError

--- telemeta/core.py
+++ telemeta/core.py
@@ -21,11 +21,11 @@ class TelemetaError(Exception):
     """Exception base class for errors in Telemeta."""
     # FIXME: is this redundant with Django's error handling ?
 
-    def __init__(self, message, title=None, show_traceback=False):
-        Exception.__init__(self, message)
-        self.message = message
-        self.title = title
-        self.show_traceback = show_traceback
+#    def __init__(self, message, title=None, show_traceback=False):
+#        Exception.__init__(self, message)
+#        self.message = message
+#        self.title = title
+#        self.show_traceback = show_traceback
 
 
 class Interface(object):
