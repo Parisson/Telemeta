0a157ed46d40f0213b5286cdaeef6345cba32263
yomguy <>
2008-09-01 13:17:52 +0000

    * Remove audiolab from source (not 2.4 compatible) * Need to install with Debian or separatly

--- telemeta/util/audiolab/scikits/audiolab/docs/examples/format1.py
+++
@@ -1,7 +0,0 @@
-from scikits.audiolab import formatinfo as format
-
-f = format('aiff', 'ulaw')
-print f
-
-f = format('ircam', 'float32')
-print f
--- telemeta/util/audiolab/scikits/audiolab/docs/examples/usage1.py
+++
@@ -1,6 +0,0 @@
-import scikits.audiolab as audiolab
-
-filename    = 'test.flac'
-a           = audiolab.sndfile(filename, 'read')
-
-print a
