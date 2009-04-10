fa6f93a6778755100a06c25aac4dff74ba57c5f4
olivier <>
2009-04-10 16:06:18 +0000

    eztelemeta: do not let sm2's page player play with the page title

--- tools/eztelemeta/design/standard/javascript/init.js
+++ tools/eztelemeta/design/standard/javascript/init.js
@@ -4,10 +4,11 @@ soundManager.debugMode = true;
 
 /* SoundManager2 Page Player configuration */
 var PP_CONFIG = {
-  flashVersion: 9,
-  usePeakData: true,
-  useWaveformData: false,
-  useEQData: false,
-  useFavIcon: false,
-  useMovieStar: false
+  flashVersion:     9,
+  usePeakData:      true,
+  useWaveformData:  false,
+  useEQData:        false,
+  useFavIcon:       false,
+  useMovieStar:     false,
+  updatePageTitle:  false
 }
