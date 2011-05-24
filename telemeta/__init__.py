91c672aa9bfb140c99ebd477a26c3107d8efc909
yomguy <yomguy@parisson.com>
2011-05-24 12:08:51 +0200

    cleanup init

--- telemeta/__init__.py
+++ telemeta/__init__.py
@@ -53,6 +53,4 @@ The fact that you are presently reading this means that you have had
 knowledge of the CeCILL license and that you accept its terms.
 """
 
-from models import *
-from util import *
 
