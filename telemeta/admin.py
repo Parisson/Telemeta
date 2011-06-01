d131c7601e38ab1de5040018cc3b86fdcd2d47e5
yomguy <yomguy@parisson.com>
2011-06-01 18:15:54 +0200

    add ordering to admin lists

--- telemeta/admin.py
+++ telemeta/admin.py
@@ -6,36 +6,43 @@ from django.contrib import admin
 
 class MediaCollectionAdmin(admin.ModelAdmin):
     search_fields = ['title', 'code']
+    ordering = ['code']
 
 class MediaItemAdmin(admin.ModelAdmin):
     search_fields = ['title', 'code']
+    ordering = ['code']
 
 class MediaPartAdmin(admin.ModelAdmin):
-    search_fields = ['title']
-
+    search_fields = ['title', 'item__code']
+    ordering = ['title']
 
 class InstrumentAdmin(admin.ModelAdmin):
-     search_fields = ['name']
-     
+    search_fields = ['name']
+    ordering = ['name']
+    
 class InstrumentAliasAdmin(admin.ModelAdmin):
-     search_fields = ['name']
+    search_fields = ['name']
+    ordering = ['name']
 
 class InstrumentRelationAdmin(admin.ModelAdmin):
-     search_fields = ['instrument__name', 'parent_instrument__name']
-
+    search_fields = ['instrument__name', 'parent_instrument__name']
+    ordering = ['parent_instrument__name']
+     
 class InstrumentAliasRelationAdmin(admin.ModelAdmin):
-     search_fields = ['alias__name', 'instrument__name']
-
+    search_fields = ['alias__name', 'instrument__name']
+    ordering = ['alias__name']
 
 class LocationAdmin(admin.ModelAdmin):
-     search_fields = ['name']
+    search_fields = ['name']
+    ordering = ['name']
      
 class LocationAliasAdmin(admin.ModelAdmin):
-     search_fields = ['location__name', 'alias']
-
+    search_fields = ['location__name', 'alias']
+    ordering = ['alias']
+     
 class LocationRelationAdmin(admin.ModelAdmin):
-     search_fields = ['location__name', 'ancestor_location__name']
-
+    search_fields = ['location__name', 'ancestor_location__name']
+    ordering = ['ancestor_location__name']
 
 admin.site.register(MediaCollection, MediaCollectionAdmin)
 #admin.site.register(MediaItem, MediaItemAdmin)
