fe378212cafdcb48215f24e4de1b212a686e98a8
yomguy <yomguy@parisson.com>
2011-06-22 14:57:07 +0200

    separate all views, fix urls, fix js trans

--- telemeta/htdocs/js/locale.js
+++ telemeta/htdocs/js/locale.js
@@ -1,3 +1,20 @@
+var localeStrings = {
+    'title': gettext('title'),
+    'description': gettext('description'),
+    'delete the marker permanently?': gettext('delete the marker permanently?'),
+    'marker added to the selected playlist': gettext('marker added to the selected playlist'),
+    'item added to the selected playlist': gettext('item added to the selected playlist'),
+    'collection added to the selected playlist': gettext('collection added to the selected playlist'),
+    'there are unsaved or modified markers': gettext('there are unsaved or modified markers'),
+    'If you exit the page you will loose your changes' : gettext('If you exit the page you will loose your changes'),
+    'author' : gettext('author')
+};
+
+function gettrans(str){
+    var loc = localeStrings; //instantiate once for faster lookup
+    return str in loc ? loc[str] : str;
+}
+
 /*
  * Copyright (C) 2007-2011 Parisson
  * Copyright (c) 2011 Riccardo Zaccarelli <riccardo.zaccarelli@gmail.com>
@@ -24,19 +41,4 @@
  * Class for managing translations in telemeta.
  */
 
-var localeStrings = {
-    'title': gettext('title'),
-    'description': gettext('description'),
-    'delete the marker permanently?': gettext('delete the marker permanently?'),
-    'marker added to the selected playlist': gettext('marker added to the selected playlist'),
-    'item added to the selected playlist': gettext('item added to the selected playlist'),
-    'collection added to the selected playlist': gettext('collection added to the selected playlist'),
-    'there are unsaved or modified markers': gettext('there are unsaved or modified markers'),
-    'If you exit the page you will loose your changes' : gettext('If you exit the page you will loose your changes'),
-    'author' : gettext('author')
-};
 
-function gettrans(str){
-    var loc = localeStrings; //instantiate once for faster lookup
-    return str in loc ? loc[str] : str;
-}
