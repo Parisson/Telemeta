a3e2afcc52ead70cc58e98f724817da0df0d468e
yomguy <yomguy@parisson.com>
2011-10-28 16:57:29 +0200

    fix description

--- setup.py
+++ setup.py
@@ -55,33 +55,5 @@ setup(
   data_files = data_files,
   intall_requires = ['timeside'],
   classifiers = ['Environment :: Web Environment', 'Framework :: Django', 'Intended Audience :: Science/Research', 'Intended Audience :: Education', 'Programming Language :: Python', 'Programming Language :: JavaScript', 'Topic :: Internet :: WWW/HTTP :: Dynamic Content', 'Topic :: Internet :: WWW/HTTP :: WSGI :: Application', 'Topic :: Multimedia :: Sound/Audio', 'Topic :: Multimedia :: Sound/Audio :: Analysis', 'Topic :: Multimedia :: Sound/Audio :: Players', 'Topic :: Scientific/Engineering :: Information Analysis', 'Topic :: System :: Archiving',  ], 
-  long_description = """
-Telemeta is a web audio archiving program which introduces useful and secure methods to backup, index, transcode, analyse and publish any digitalized audio file with its metadata. It is dedicated to professionnals who wants to easily backup and publish documented sounds from collections of vinyls, magnetic tapes or audio CDs over a strong database, in accordance with open standards.
-
-Here are the main features of Telemeta:
-
- * Secure archiving, editing and publishing of audio files over internet.
- * User friendly web frontend including workflows and high level search methods
- * Smart dynamical and skinnable audio player (thanks to TimeSide and SoundManager2)
- * "On the fly" analyzing, transcoding and metadata embedding based on an easy plugin architecture
- * Temporal and collaborative indexation with fast user marker management
- * Multi-format support : FLAC, OGG, MP3, WAV and more
- * User management with individual profiles and rights
- * Playlist management for users with CSV data export
- * Geo-Navigator for audio geolocalization
- * DublinCore compatibility
- * OAI-PMH data provider
- * RSS feeds generator
- * XML serialized backup
- * Strong SQL or Oracle backend
-
-The Telemeta data model is now based on *collection* and *item* elements. 
-A *collection* is described by its metadata and is related to audio *items*. 
-An *item* embeds audio files with its metadata. 
-This model has been designed to fit the one of the French Centre of Ethnomusicology 
-(`CREM <http://www.crem-cnrs.fr>`_) of the University of Paris Ouest - Nanterre 
-but could be easily adapted or overrided to suit other data structures.
-
-See http://telemeta.org for more informations.
-"""
-)
+  long_description = open('README.rst').read()
+  )
