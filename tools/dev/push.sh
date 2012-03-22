28fa06bf582846fa98a01136ed79a1536b57c61d
yomguy <yomguy@parisson.com>
2012-03-22 02:55:52 +0100

    fix thumbs height

--- tools/dev/push.sh
+++ tools/dev/push.sh
@@ -7,9 +7,6 @@ do
     echo "Push $branch to origin:"
     git push origin $branch
 
-    echo "Push $branch to web:"
-    git push web $branch
-
     if [[ $branch == *master* ]]; then
         echo "Push $branch to github:"
         git push github $branch
@@ -18,18 +15,16 @@ do
 done
 
 git push --tags
-git push --tags web
 git push --tags github
 
 ssh vcs.parisson.com "cd /var/git/telemeta.git; git update-server-info"
-ssh vcs.parisson.org "cd /var/git/telemeta.git; git update-server-info"
 
 #echo "Update jimi.parisson.com:"
-#ssh jimi.parisson.com "cd /home/telemeta/telemeta; git pull origin production"
 echo "Update angus.parisson.com:"
 ssh angus.parisson.com "cd /home/telemeta/telemeta-prod; git pull origin production; \
                         cd /home/telemeta/telemeta; git pull origin master; \
                         cd /home/telemeta/demo/; ./manage.py migrate telemeta --delete-ghost-migrations;
-                        cd /home/telemeta/sandbox/; ./manage.py migrate telemeta --delete-ghost-migrations; "
+                        cd /home/telemeta/sandbox/; ./manage.py migrate telemeta --delete-ghost-migrations; 
+                        cd /home/telemeta/parisson/; ./manage.py migrate telemeta --delete-ghost-migrations; "
 
 echo "Done !"
