bf8dc4f7ed41d0de59cc0fb4bf7c8a9875666700
yomguy <yomguy@parisson.com>
2012-03-26 14:15:24 +0200

    update graphs

--- example/sandbox_sqlite/diag.sh
+++ example/sandbox_sqlite/diag.sh
@@ -9,3 +9,4 @@ python modelviz.py $app > $dir/$app.dot
 dot $dir/$app-all.dot -Tpdf -o $dir/$app-all.pdf
 dot $dir/$app.dot -Tpdf -o $dir/$app.pdf
 
+rsync -a $dir/ doc.parisson.com:/var/www/files/doc/$app/diagram/
