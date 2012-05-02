03ca02316c633f89a5c1fcb7b8e1af2a6397ee0d
yomguy <yomguy@parisson.com>
2012-05-02 16:31:02 +0200

    fix format

--- telemeta/views/base.py
+++ telemeta/views/base.py
@@ -659,7 +659,7 @@ class ItemView(object):
         else:
             last_revision = None
 
-        physical_format = item.format.physical_format
+        physical_format = item.original_format.original_format
 
         return render(request, template,
                     {'item': item, 'export_formats': formats,
