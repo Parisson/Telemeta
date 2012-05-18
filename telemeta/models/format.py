f84855f9dbdf0f6a4caf586d0dc36d77d2ae1a42
yomguy <yomguy@parisson.com>
2012-05-18 11:28:45 +0200

    add item to format

--- telemeta/models/format.py
+++ telemeta/models/format.py
@@ -54,6 +54,8 @@ from django.db import models
 class Format(ModelCore):
     """ Physical format object as proposed by the LAM"""
 
+    item            = ForeignKey(Item, related_name="format",
+                                     verbose_name = _("item") )
     original_format = WeakForeignKey(OriginalFormat, related_name="format",
                                      verbose_name = _("original format"))
     original_code = CharField(_('original code'), required=True)
