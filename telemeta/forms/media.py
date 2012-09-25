7439ba46418c9d15303390e4aa14b72b1b5014c1
yomguy <yomguy@parisson.com>
2012-09-25 14:32:24 +0200

    fix double old_code, add keywords

--- telemeta/forms/media.py
+++ telemeta/forms/media.py
@@ -72,7 +72,7 @@ class MediaItemForm(ModelForm):
         exclude = ('alt_title', 'copied_from_item', 'cultural_area', 'ethnic_group', 'language',
                    'context_comment', 'moda_execut', 'vernacular_style', 'generic_style',
                    'track', 'collector', 'collector_selection', 'collector_from_collection',
-                   'creator_reference')
+                   'creator_reference', 'old_code')
 
     def clean_code(self):
         return self.cleaned_data['code'] or None
