5facf059045fc128e05fea35eb9bf5f30a2e1526
yomguy <yomguy@parisson.com>
2012-10-02 14:19:03 +0200

    * delete timeside static files, please ADD 'timeside' to INSTALLED_APPS * cleanup

--- example/sandbox_mysql/settings.py
+++ example/sandbox_mysql/settings.py
@@ -63,7 +63,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/home/dev/telemeta/static/'
+STATIC_ROOT = '/var/www/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -122,6 +122,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.staticfiles',
     'telemeta',
+    'timeside',
     'jsonrpc',
     'south',
     'sorl.thumbnail',
--- example/sandbox_sqlite/settings.py
+++ example/sandbox_sqlite/settings.py
@@ -66,7 +66,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/home/dev/telemeta/static/'
+STATIC_ROOT = '/var/www/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -125,6 +125,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django_extensions',
     'telemeta',
+    'timeside',
     'jsonrpc',
     'south',
     'sorl.thumbnail',
