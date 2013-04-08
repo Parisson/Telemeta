81851b4144f46a23dd909d25e867b403dd2682b8
Guillaume Pellerin <yomguy@parisson.com>
2013-04-08 12:28:17 +0200

    add kdenlive scripts from promaster

---
+++ telemeta/util/kdenlive/auto_fade_batch.py
@@ -0,0 +1,19 @@
+
+import os, sys
+from fade import AutoFade
+
+if __name__ == '__main__':
+    dir = sys.argv[-2]
+    ext = sys.argv[-1]
+
+    for filename in os.listdir(dir):
+        prefix, extension = os.path.splitext(filename)
+        path = dir + os.sep + filename
+        flag = path + '.faded'
+        if ext in extension and not os.path.exists(flag):
+            fade = AutoFade(path)
+            data = fade.run()
+            f = open(path, 'w')
+            f.write(data)
+            f.close()
+            os.system('touch ' + flag)
