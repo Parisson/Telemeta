a47e5baa09c21b66e5f774c0a7b60dbb6f5aa92b
yomguy <yomguy@parisson.com>
2013-04-08 15:21:45 +0200

    fix sudo

--- telemeta/util/kdenlive/mlt_fix_threads.sh
+++ telemeta/util/kdenlive/mlt_fix_threads.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 for file in `ls $1/*.sh`; do
-sudo perl -pi -e 's/threads=2/threads=8/g' $1/$file 
-sudo perl -pi -e 's/threads=4/threads=8/g' $1/$file
-sudo perl -pi -e 's/threads=6/threads=8/g' $1/$file
+perl -pi -e 's/threads=2/threads=8/g' $1/$file
+perl -pi -e 's/threads=4/threads=8/g' $1/$file
+perl -pi -e 's/threads=6/threads=8/g' $1/$file
 done
