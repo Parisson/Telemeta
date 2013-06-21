a92425791cc282cafbf19323917d2c19d6c8efd5
Guillaume Pellerin <yomguy@parisson.com>
2013-06-21 21:22:11 +0200

    add fading kdenlive rendering batch

--- telemeta/util/kdenlive/mlt_process_batch.py
+++ telemeta/util/kdenlive/mlt_process_batch.py
@@ -2,12 +2,28 @@
 import os, sys
 
 if __name__ == '__main__':
-    dir = sys.argv[-1]
+    root_dir = sys.argv[-1]
 
-    for filename in os.listdir(dir):
-        prefix, extension = os.path.splitext(filename)
-        path = dir + os.sep + filename
-        flag = path + '.processed'
-        if 'sh' in extension and not os.path.exists(flag):
-            os.system('nice -n 19 ' + path)
-            os.system('touch ' + flag)
+    fading = False
+    if '--fade' in sys.argv:
+        fading = True
+
+    for root, dirs, files in os.walk(root_dir):
+        for filename in files:
+            prefix, extension = os.path.splitext(filename)
+            path = root + os.sep + filename
+
+            flag = path + '.faded'
+            if 'mlt' in extension and not os.path.exists(flag) and fading:
+                from telemeta.util.kdenlive.fade import AutoFade
+                fade = AutoFade(path)
+                data = fade.run()
+                f = open(path, 'w')
+                f.write(data)
+                f.close()
+                os.system('touch ' + flag)
+
+            flag = path + '.processed'
+            if 'sh' in extension and not os.path.exists(flag):
+                os.system('nice -n 19 ' + path)
+                os.system('touch ' + flag)
