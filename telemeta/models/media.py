d047a7f8c9b19f7cd64b35a8b699bcdc01520b74
Guillaume Pellerin <yomguy@parisson.com>
2013-12-31 00:13:02 +0100

    add has_mediafile (sound) to all resources

--- telemeta/models/media.py
+++ telemeta/models/media.py
@@ -775,6 +775,13 @@ class MediaCorpus(MediaBaseResource):
     @property
     def public_id(self):
         return self.code
+    
+    @property
+    def has_mediafile(self):
+        for child in self.children.all():
+            if child.has_mediafile:
+                return True
+        return False
 
     class Meta(MetaCore):
         db_table = 'media_corpus'
@@ -796,6 +803,13 @@ class MediaFonds(MediaBaseResource):
     @property
     def public_id(self):
         return self.code
+
+    @property
+    def has_mediafile(self):
+        for child in self.children.all():
+            if child.has_mediafile:
+                return True
+        return False
 
     class Meta(MetaCore):
         db_table = 'media_fonds'
