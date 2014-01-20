1446c1b874d201b9a902499d66859d63d665b3c1
Guillaume Pellerin <yomguy@parisson.com>
2014-01-21 00:20:31 +0100

    fix no debug toolbar

--- telemeta/urls.py
+++ telemeta/urls.py
@@ -364,6 +364,10 @@ urlpatterns = patterns('',
 )
 
 if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns += patterns('',
-    url(r'^__debug__/', include(debug_toolbar.urls)),)
+    try:
+        import debug_toolbar
+        urlpatterns += patterns('',
+        url(r'^__debug__/', include(debug_toolbar.urls)),)
+    except:
+        pass
+
