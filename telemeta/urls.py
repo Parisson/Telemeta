21d20d91a79a0239859b31f8a5f428f495b9b890
Guillaume Pellerin <yomguy@parisson.com>
2014-05-14 23:56:21 +0200

    add generic user profile url (fix #171)

--- telemeta/urls.py
+++ telemeta/urls.py
@@ -331,6 +331,7 @@ urlpatterns = patterns('',
 
     # Profiles
     url(r'^accounts/(?P<username>[A-Za-z0-9._-]+)/profile/$', profile_view.profile_detail, name="telemeta-profile-detail"),
+    url(r'^users/(?P<username>[A-Za-z0-9._-]+)/$', profile_view.profile_detail, name="telemeta-profile-detail-2"),
     url(r'^accounts/(?P<username>[A-Za-z0-9._-]+)/profile/edit/$', profile_view.profile_edit, name="telemeta-profile-edit"),
     url(r'^accounts/(?P<username>[A-Za-z0-9._-]+)/rss/$', UserRevisionsFeed(),  name="telemeta-user-rss"),
 
