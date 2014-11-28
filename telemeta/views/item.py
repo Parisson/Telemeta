dbf6ac4f6b396239c36aae4a8ae8d10fa2982e37
Guillaume Pellerin <yomguy@parisson.com>
2014-11-28 02:12:09 +0100

    fix copy

--- telemeta/views/item.py
+++ telemeta/views/item.py
@@ -778,47 +778,22 @@ class ItemCopyView(ItemAddView):
     def get_initial(self):
         return model_to_dict(self.get_object())
 
-    def form_valid(self, form):
-        item = MediaItem()
-        if self.request.FILES:
-            item_form = MediaItemForm(data=self.request.POST, files=self.request.FILES, instance=item, prefix='item')
-        else:
-            item_form = MediaItemForm(data=self.request.POST, instance=item, prefix='item')
-
-        if item_form.is_valid():
-            item_form.save()
-            if not self.request.FILES:
-                item.file = source_item.file
-                item.save()
-
-            code = item_form.cleaned_data['code']
-            if not code:
-                code = str(item.id)
-            if format_form.is_valid():
-                format.item = item
-                format_form.save()
-
-            performances = MediaItemPerformance.objects.filter(media_item=source_item)
-            for performance in performances:
-                performance.pk = None
-                performance.id = None
-                performance.media_item = item
-                performance.save()
-
-            keywords = MediaItemKeyword.objects.filter(item=source_item)
-            for keyword in keywords:
-                keyword.pk = None
-                keyword.id = None
-                keyword.item = item
-                keyword.save()
-
-            item.set_revision(self.request.user)
-
-        self.code = form.cleaned_data['code']
-        return super(ItemCopyView, self).form_valid(form)
-
     def get_success_url(self):
-        return reverse_lazy('telemeta-item-detail', kwargs={'public_id':self.code})
+        return reverse_lazy('telemeta-item-detail', kwargs={'public_id':self.object.code})
+
+    def get_context_data(self, **kwargs):
+        context = super(ItemCopyView, self).get_context_data(**kwargs)
+        item = self.get_object()
+        context['item'] = item
+        context['access'] = get_item_access(item, self.request.user)
+        context['previous'], context['next'] = self.item_previous_next(item)
+        #FIXME
+        context['mime_type'] = 'audio/mp3'
+        context['export_formats'] = self.get_export_formats()
+        context['visualizers'] = self.get_graphers()
+        context['audio_export_enabled'] = self.export_enabled
+        context['auto_zoom'] = True
+        return context
 
 
 class ItemDetailView(ItemViewMixin, DetailView):
