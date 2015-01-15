c5165ed9fa9e2ddfd0088691054271bd7396144a
Guillaume Pellerin <yomguy@parisson.com>
2015-01-15 11:14:54 +0100

    bugfix

--- telemeta/models/media.py
+++ telemeta/models/media.py
@@ -363,7 +363,7 @@ class MediaCollection(MediaResource):
         # metadata = model_to_dict(self, fields=[], exclude=self.exclude)
         metadata = self.to_dict()
         for key in self.exclude:
-            metadata.remove(key)
+            del metadata[key]
         metadata['url'] = get_full_url(reverse('telemeta-collection-detail', kwargs={'public_id':self.pk}))
         metadata['doc_status'] = self.document_status()
         metadata['countries'] = ';'.join([location.name for location in self.main_countries()])
@@ -594,7 +594,7 @@ class MediaItem(MediaResource):
         # metadata = model_to_dict(self, fields=[], exclude=self.exclude)
         metadata = self.to_dict()
         for key in self.exclude:
-            metadata.remove(key)
+            del metadata[key]
         metadata['url'] = self.get_url()
         metadata['last_modification_date'] = unicode(self.get_revision().time)
         metadata['collection'] = self.collection.get_url()
