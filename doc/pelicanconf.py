5093c9a5a09dc9a6c28df05ff95b51bb2ab011aa
Guillaume Pellerin <yomguy@parisson.com>
2015-04-09 17:37:35 +0200

    update blog config, add LANGUAGE_BIDI to base.html

--- doc/pelicanconf.py
+++ doc/pelicanconf.py
@@ -3,36 +3,35 @@
 from __future__ import unicode_literals
 
 AUTHOR = u'Parisson Sarl'
-SITENAME = u'Telemeta'
-#SITEURL = ''
+SITENAME = u'Telemeta project'
+
+TIMEZONE = 'Europe/Paris'
+DEFAULT_LANG = u'en'
+DEFAULT_DATE = 'fs'
+
+SITEURL = 'http://parisson.github.io/Telemeta/'
 RELATIVE_URLS = True
 
 PATH = 'content/'
+DELETE_OUTPUT_DIRECTORY = False
 
 THEME = 'themes/pelican-bootstrap3'
 BOOTSTRAP_THEME = 'united'
-
-TIMEZONE = 'Europe/Paris'
-
-DEFAULT_LANG = u'en'
-DEFAULT_DATE = 'fs'
 
 SUMMARY_MAX_LENGTH = 127
 SLUGIFY_SOURCE = 'title'
 DEFAULT_PAGINATION = 10
 
-
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS =  (('Pelican', 'http://getpelican.com/'),
-          ('Python', 'http://python.org/'),
+LINKS =  (('Python', 'http://python.org/'),
           ('Django', 'https://www.djangoproject.com/'),
+          ('TimeSide', 'https://github.com/Parisson/TimeSide'),
+          ('Docker', 'https://www.docker.com/')
           )
 
 # Social widget
@@ -45,9 +44,25 @@ SOCIAL = (('Twitter', 'https://twitter.com/telemeta/'),
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
-# PLUGIN_PATHS = ['plugins']
-# PLUGINS = ['pin_to_top']
+PLUGIN_PATHS = ['plugins']
+PLUGINS = ['pin_to_top']
 
 STATIC_PATHS = ['images', 'css']
 CUSTOM_CSS = 'css/custom.css'
 # SITELOGO = '/images/logo_telemeta_2.png'
+EXTRA_PATH_METADATA = {
+    #'robots.txt': {'path': 'robots.txt'},
+    'images/favicon.ico': {'path': 'favicon.ico'}
+}
+
+DISQUS_SITENAME='telemetaorg'
+
+TWITTER_CARDS = True
+TWITTER_USERNAME = 'telemeta'
+TWITTER_WIDGET_ID = '585766293153968128'
+
+GITHUB_USER = 'Parisson'
+
+# Content licensing: CC-BY
+CC_LICENSE = "CC-BY"
+
