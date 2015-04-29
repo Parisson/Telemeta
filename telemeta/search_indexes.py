e00f178465d859c902ca627e701e8d6d70f26eee
Kaltar5679 <killian.mary@outlook.fr>
2015-04-29 11:10:07 +0200

    Advance search add ethnic_group

--- telemeta/search_indexes.py
+++ telemeta/search_indexes.py
@@ -7,10 +7,13 @@ class MediaItemIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
     item_acces = indexes.CharField(model_attr='collection__public_access', faceted=True)
     item_status = indexes.CharField(model_attr='collection__document_status', faceted=True)
+    digitized = indexes.BooleanField(default=False, faceted=True)
+
+    #advance search
     title = indexes.NgramField(model_attr='title')
     code = indexes.NgramField(model_attr='code')
     location = indexes.NgramField(model_attr='location__name', default='')
-    digitized = indexes.BooleanField(default=False, faceted=True)
+    ethnic_group = indexes.NgramField(model_attr='ethnic_group', default='')
 
     def prepare_digitized(self, obj):
         if obj.file.name:
@@ -28,12 +31,15 @@ class MediaCollectionIndex(indexes.SearchIndex, indexes.Indexable):
 
     text = indexes.NgramField(document=True, use_template=True)
     #rec_date = indexes.DateTimeField(use_template=True, null=True)
-    title = indexes.NgramField(model_attr='title')
-    code = indexes.NgramField(model_attr='code')
-    location = indexes.NgramField(default='')
     item_acces = indexes.CharField(model_attr='public_access', faceted=True)
     item_status = indexes.CharField(model_attr='document_status', faceted=True)
     digitized = indexes.BooleanField(default=False, faceted=True)
+
+    #advance search
+    title = indexes.NgramField(model_attr='title')
+    code = indexes.NgramField(model_attr='code')
+    location = indexes.NgramField(default='')
+    ethnic_group = indexes.NgramField(default='')
 
     def prepare_digitized(self, obj):
         return obj.has_mediafile
@@ -43,3 +49,6 @@ class MediaCollectionIndex(indexes.SearchIndex, indexes.Indexable):
 
     def prepare_location(self, obj):
         return "%s" % obj.countries()
+
+    def prepare_ethnic_group(self, obj):
+        return "%s" % obj.ethnic_groups()
--- telemeta/views/haystack_search.py
+++ telemeta/views/haystack_search.py
@@ -68,16 +68,6 @@ class HaystackAdvanceSearch(SearchView):
 
     def extra_context(self):
         extra = super(HaystackAdvanceSearch, self).extra_context()
-
-        if self.request.GET.get('q'):
-            extra['title'] = self.request.GET['q']
-
-        if self.request.GET.get('code'):
-            extra['code'] = self.request.GET['code']
-
-        if self.request.GET.get('location'):
-            extra['location'] = self.request.GET['location']
-
         extra['collection_count'] = self.form.search().models(MediaCollection).count()
         extra['item_count'] = self.form.search().models(MediaItem).count()
         if self.type == 'collection':
