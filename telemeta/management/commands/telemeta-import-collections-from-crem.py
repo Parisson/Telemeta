73049c0d10bf414837918d7e7659e90599ede5e8
Guillaume Pellerin <yomguy@parisson.com>
2015-05-07 09:16:51 +0200

    fix import

--- telemeta/management/commands/telemeta-import-collections-from-crem.py
+++ telemeta/management/commands/telemeta-import-collections-from-crem.py
@@ -23,7 +23,6 @@ from optparse import make_option
 from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth.models import User
-from django.core.management import setup_environ
 from django.core.files.base import ContentFile
 from django.contrib.auth.models import User
 from django.contrib.sites.models import Site
