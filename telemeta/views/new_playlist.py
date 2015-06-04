4035fc08cf16d0a600732cb1fed647e2023db286
afilsaime <shadow_kungfu@hotmail.fr>
2015-06-04 14:18:53 +0200

    implementing the facet selection for user playlist

--- telemeta/views/new_playlist.py
+++ telemeta/views/new_playlist.py
@@ -2,15 +2,38 @@
 from telemeta.views.core import *
 from telemeta.models import *
 
-class NewPlaylistView(TemplateView):
-    template_name = 'search/addplaylist.html'
-    def get_context_data(self, **kwargs):
-        # Call the base implementation first to get a context
-        context = super(NewPlaylistView, self).get_context_data(**kwargs)
-        # Add in the publisher
-        idlist = self.request.GET.getlist('selected_items_list')
+class NewPlaylistView(object):
+
+    def display(self, request):
+
+        template_name = loader.get_template('search/addplaylist.html')
+
+        idlist = request.POST.getlist('selected_items_list')
         itemlist = []
         for itemid in idlist:
             itemlist.append(MediaItem.objects.all().get(id=itemid))
-        context['selected_items_list'] = itemlist
-        return context
+        context = RequestContext(request, {
+                'selected_items_list': itemlist,
+                'existing_playlists': Playlist.objects.all()})
+        return HttpResponse(template_name.render(context))
+
+    def addToPlaylist(self, request):
+        template_name = loader.get_template('search/confirmation_add_playslist.html')
+
+        idlist = request.POST.getlist('item_id')
+        selected_playlist_id = request.POST.get('playlist_id')
+        selected_playlist = Playlist.objects.all().get(id=selected_playlist_id)
+
+        itemlist = []
+        for itemid in idlist:
+            itemlist.append(MediaItem.objects.all().get(id=itemid))
+
+        for item in itemlist:
+            resource = PlaylistResource(resource_type='item',public_id='4567891542',resource_id=item.id,playlist=selected_playlist)
+            resource.save()
+
+        context = RequestContext(request, {
+                'existing_playlists': idlist})
+        return HttpResponse(template_name.render(context))
+
+
