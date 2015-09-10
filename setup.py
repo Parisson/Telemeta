4b1e1218abb380d357dca0e5923a2794f62eae8f
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2015-09-10 14:41:22 +0200

    goto 1.6

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
   long_description = open('README.rst').read(),
   author = "Guillaume Pellerin",
   author_email = "yomguy@parisson.com",
-  version = '1.5.2',
+  version = '1.6',
   install_requires = [
         'django==1.6.11',
         'django-registration',
