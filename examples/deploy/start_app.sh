ad5bc3abecbb1bdb58f68e690a706912534a7076
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2015-12-08 17:07:14 +0100

    fix bower install

--- examples/deploy/start_app.sh
+++ examples/deploy/start_app.sh
@@ -8,7 +8,7 @@ manage=$sandbox'/manage.py'
 wsgi=$sandbox'/wsgi.py'
 
 # stating apps
-pip install django-haystack elasticsearch
+pip install django-haystack elasticsearch django-bower
 
 # waiting for other services
 sh $app_dir/examples/deploy/wait.sh
@@ -16,8 +16,8 @@ sh $app_dir/examples/deploy/wait.sh
 # django init
 python $manage syncdb --noinput
 python $manage migrate --noinput
-python $manage collectstatic --noinput
 python $manage bower install --noinput
+python $manage collectstatic --noinput
 python $manage telemeta-create-admin-user
 python $manage telemeta-create-boilerplate
 python $manage update_index
