bb69fca4c621dda52237347109d5a4669badc4e1
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2015-12-08 17:04:37 +0100

    add bower install to start_app

--- examples/deploy/start_app.sh
+++ examples/deploy/start_app.sh
@@ -17,6 +17,7 @@ sh $app_dir/examples/deploy/wait.sh
 python $manage syncdb --noinput
 python $manage migrate --noinput
 python $manage collectstatic --noinput
+python $manage bower install --noinput
 python $manage telemeta-create-admin-user
 python $manage telemeta-create-boilerplate
 python $manage update_index
