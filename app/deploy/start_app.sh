4f0cc37137253262cc643535a440e16b1deba527
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2015-12-23 01:11:39 +0100

    cleanup, try to get phpMyAdmin working behind relative path...

--- app/deploy/start_app.sh
+++ app/deploy/start_app.sh
@@ -23,16 +23,9 @@ python $manage syncdb --noinput
 python $manage migrate --noinput
 python $manage collectstatic --noinput
 
-if [ ! -f $app/.init ]; then
- python $manage telemeta-create-admin-user
- python $manage telemeta-create-boilerplate
- python $manage update_index --workers $processes
- touch $app/.init
-fi
-
 # static files auto update
 watchmedo shell-command --patterns="*.js;*.css" --recursive \
-    --command='python '$manage' collectstatic --noinput' $static &  
+    --command='python '$manage' collectstatic --noinput' $static &
 
 # app start
 uwsgi --socket :$port --wsgi-file $wsgi --chdir $app --master --processes $processes --threads $threads --py-autoreload $autoreload
