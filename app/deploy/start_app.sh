5ef4f3695677381bd993f9247da8029135db3959
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2016-01-05 13:15:34 +0100

    set uwsgi uid and gid to www-data

--- app/deploy/start_app.sh
+++ app/deploy/start_app.sh
@@ -11,6 +11,8 @@ port=8000
 processes=8
 threads=8
 autoreload=3
+uid='www-data'
+gid='www-data'
 
 # stating apps
 pip install redis
@@ -35,4 +37,7 @@ watchmedo shell-command --patterns="*.js;*.css" --recursive \
     --command='python '$manage' collectstatic --noinput' $static &
 
 # app start
-uwsgi --socket :$port --wsgi-file $wsgi --chdir $app --master --processes $processes --threads $threads --py-autoreload $autoreload
+uwsgi --socket :$port --wsgi-file $wsgi --chdir $app --master \
+    --processes $processes --threads $threads \
+    --uid $uid --gid $gid \
+    --py-autoreload $autoreload
