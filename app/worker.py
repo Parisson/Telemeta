a9a6f576fd1d5cf2480ab858413a9de81979aaf1
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2016-04-14 00:45:14 +0200

    fix init, use bash

--- app/worker.py
+++ app/worker.py
@@ -17,7 +17,7 @@ app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 # app.conf.update(
 #     CELERY_RESULT_BACKEND='djcelery.backends.database:DatabaseBackend',
-# )
+# ) 
 
 @app.task(bind=True)
 def debug_task(self):
