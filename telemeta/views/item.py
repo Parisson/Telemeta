e291292633423cb5926cd1c45efbcb5d54fcb5b8
Thomas Fillon <thomas@parisson.com>
2016-05-12 01:42:44 +0200

    Remove TimeSide grapher computation through pipe

--- telemeta/views/item.py
+++ telemeta/views/item.py
@@ -27,6 +27,8 @@ from telemeta.views.marker import *
 import timeside.core
 import timeside.server as ts
 
+import time
+from wsgiref.util import FileWrapper
 
 class ItemBaseMixin(TelemetaBaseMixin):
 
@@ -250,36 +252,17 @@ class ItemView(ItemBaseMixin):
         ts_experience = ts_preset.get_single_experience()
         ts_selection = ts_item.get_single_selection()
         ts_task, c = ts.models.Task.objects.get_or_create(experience=ts_experience,
-                                                   selection=ts_selection)
+                                                          selection=ts_selection)
         ts_task.run()
 
-        grapher = self.get_grapher(grapher_id)
+        status = ts.models.Task.objects.get(id=ts_task.id).status
+        while (status != ts.models._DONE):
+            time.sleep(0.5)
+            status = ts.models.Task.objects.get(id=ts_task.id).status
+            
+        result = ts.models.Result.objects.get(preset=ts_preset, item=ts_item)
 
-        if grapher.id() != grapher_id:
-            raise Http404
-
-        size = str(width) + '_' + str(height)
-        image_file = '.'.join([public_id, grapher_id, size, 'png'])
-
-        # FIX waveform grapher name change
-        old_image_file = '.'.join([public_id, 'waveform', size, 'png'])
-        if 'waveform_centroid' in grapher_id and self.cache_data.exists(old_image_file):
-            image_file = old_image_file
-
-        if not self.cache_data.exists(image_file):
-            source, _ = item.get_source()
-            if source:
-                path = self.cache_data.dir + os.sep + image_file
-                decoder = timeside.core.get_processor('file_decoder')(source)
-                graph = grapher(width=width, height=height)
-                (decoder | graph).run()
-                graph.watermark('timeside', opacity=.6, margin=(5,5))
-                f = open(path, 'w')
-                graph.render(output=path)
-                f.close()
-                self.cache_data.add_file(image_file)
-
-        response = StreamingHttpResponse(self.cache_data.read_stream_bin(image_file), content_type=mime_type)
+        response = StreamingHttpResponse(FileWrapper(open(result.file.name)), content_type=mime_type)
         return response
 
     def list_export_extensions(self):
