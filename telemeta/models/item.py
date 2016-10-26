987c2aa7228befb7486ba75099815811a1625e8d
Luc LEGER <leger.luc@gmail.com>
2016-10-26 22:46:48 +0200

    issue #38 [items] Format témoignage : champ Lieux cités

--- telemeta/models/item.py
+++ telemeta/models/item.py
@@ -112,6 +112,7 @@ class MediaItem(MediaResource):
     mshs_te_details       = CharField(_('details'))
     mshs_te_thematic      = CharField(_('thematic'))
     mshs_te_names         = CharField(_('names'), help_text=_('Name; name; ...'))
+    mshs_te_places        = CharField(_('places'), help_text=_('Place named; place named; ...'))
     mshs_mu_title         = CharField(_('title'))
     mshs_mu_alt_title     = CharField(_('alternate title'))
     mshs_mu_details       = CharField(_('details on music'))
