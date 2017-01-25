5ff96863eca8813b88826a6a2400c525a1d4952c
Thomas Fillon <thomas@parisson.com>
2017-01-25 14:42:30 +0100

    Restore SITE_ID in settings

--- app/settings.py
+++ app/settings.py
@@ -61,6 +61,8 @@ LANGUAGES = [ ('fr', 'French'),
               ('es', 'Spanish'),
 ]
 
+SITE_ID = 1
+
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
 USE_I18N = True
