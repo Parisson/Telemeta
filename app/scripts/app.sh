99671fb2a58a8d89b4daabdfe9a011efb5bfe781
Thomas Fillon <thomas@parisson.com>
2017-01-27 23:46:37 +0100

    Go to django 1.9

--- app/scripts/app.sh
+++ app/scripts/app.sh
@@ -18,8 +18,8 @@ uid='www-data'
 gid='www-data'
 
 # stating apps
-#pip install -U django==1.9.12
-pip install -U django==1.8.17
+pip install -U django==1.9.*
+#pip install -U django==1.8.17
 pip uninstall -y south
 pip install -e git+https://github.com/Parisson/django-jqchat.git@dj1.8#egg=django-jqchat
 pip install django-debug-toolbar==1.6
