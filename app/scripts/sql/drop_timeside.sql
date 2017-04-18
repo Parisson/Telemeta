627773d65a19596c5d57f366e00442c4c5f82359
Guillaume Pellerin <yomguy@parisson.com>
2017-04-18 17:41:40 +0200

    Add sql script to drop Timeside server tables

---
+++ app/scripts/sql/drop_timeside.sql
@@ -0,0 +1,14 @@
+BEGIN;
+DROP TABLE `timeside_experiences`;
+DROP TABLE `timeside_results`;
+DROP TABLE `timeside_experiences_presets`;
+DROP TABLE `timeside_items`;
+DROP TABLE `timeside_selections_items`;
+DROP TABLE `timeside_experiences_experiences`;
+DROP TABLE `timeside_tasks`;
+DROP TABLE `timeside_selections_selections`;
+DROP TABLE `timeside_selections`;
+DROP TABLE `timeside_processors`;
+DROP TABLE `timeside_presets`;
+
+COMMIT;
