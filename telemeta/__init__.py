1a7e7a2079941f24d33246e4574d2b1f38d00718
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2017-06-02 13:36:44 +0200

    Goto 1.6.4

--- telemeta/__init__.py
+++ telemeta/__init__.py
@@ -15,12 +15,12 @@ U{http://telemeta.org}
 """
 
 __docformat__ = 'epytext en'
-__version__ = '1.6.3'
+__version__ = '1.6.4'
 __url__ = 'http://telemeta.org'
-__copyright__ = '(C) 2006-2016 Parisson SARL'
+__copyright__ = '(C) 2006-2017 Parisson SARL'
 __license__ = 'AGPLv3'
 __license_long__ = """
-Copyright (C) Parisson SARL (2006-2016)
+Copyright (C) Parisson SARL (2006-2017)
 
 This file is part of Telemeta.
 
