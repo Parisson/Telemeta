4a7c214da2cfd0def2e92b009c82b96003d7c0b3
Thomas Fillon <thomas@parisson.com>
2017-07-18 10:06:02 +0200

    Migrating old meta API for models.core
    see https://docs.djangoproject.com/en/1.8/ref/models/meta/#migrating-old-meta-api

--- telemeta/models/core.py
+++ telemeta/models/core.py
@@ -77,7 +77,10 @@ class EnhancedQuerySet(models.query.QuerySet):
 
     def delete(self):
         CHUNK=1024
-        objects = self.model._meta.get_all_related_objects()
+        objects = [f for f in self.model._meta.get_fields()
+                   if (f.one_to_many or f.one_to_one)
+                   and f.auto_created and not f.concrete
+        ]
         ii = self.count()
         values = self.values_list('pk')
         for related in objects:
