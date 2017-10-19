14aab3a7d6c6e3871ccae87698d35c512620fe3d
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2017-10-19 15:59:01 +0200

    split 0005 into 2 migration so that --fake 0006 can be applied against old 0036

--- telemeta/migrations/0005_auto_20170919_1521.py
+++ telemeta/migrations/0005_auto_20170919_1521.py
@@ -14,20 +14,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.CreateModel(
-            name='EnumerationProperty',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('enumeration_name', models.CharField(max_length=255, verbose_name='enumeration name')),
-                ('is_hidden', telemeta.models.fields.BooleanField(default=False, verbose_name='is hidden')),
-                ('is_admin', telemeta.models.fields.BooleanField(default=True, verbose_name='is admin')),
-            ],
-            options={
-                'verbose_name': 'enumeration property',
-                'verbose_name_plural': 'enumeration properties',
-            },
-            bases=(models.Model, dirtyfields.dirtyfields.DirtyFieldsMixin),
-        ),
         migrations.AlterField(
             model_name='mediacorpus',
             name='code',
