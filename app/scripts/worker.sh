3aea76f13200f0a35663929650a6008ab7f27aa3
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2018-05-11 15:20:23 +0200

    Set the worker concurrency

--- app/scripts/worker.sh
+++ app/scripts/worker.sh
@@ -4,6 +4,7 @@
 app='/srv/app'
 manage=$app'/manage.py'
 wsgi=$app'/wsgi.py'
+concurrency=12
 
 # stating apps
 pip uninstall -y south
@@ -12,11 +13,10 @@ pip install django-debug-toolbar==1.6
 pip install -e git+https://github.com/Parisson/django-jqchat.git@dj1.8#egg=django-jqchat
 pip install -e git+https://github.com/Parisson/saved_searches.git@dj1.8#egg=saved_searches-2.0.0-beta
 
-
 # waiting for other services
 bash $app/scripts/wait.sh
 
 # Starting celery worker with the --autoreload option will enable the worker to watch for file system changes
 # This is an experimental feature intended for use in development only
 # see http://celery.readthedocs.org/en/latest/userguide/workers.html#autoreloading
-python $manage celery worker --autoreload -A worker
+python $manage celery worker --autoreload -A worker --concurrency=$concurrency
